30ba6c562ef5facf0806dae39b0c685c12bdc317
Thomss <80680795+Gioav@users.noreply.github.com>
2025-10-20 13:02:11 +0200

    server.c

--- server.c
+++ server.c
@@ -17,7 +17,7 @@ static _Atomic unsigned int cli_count = 0;
 static int uid = 10;
 
 /* Client structure */
-typedef struct{
+typedef struct {
     struct sockaddr_in address;
     int sockfd;
     int uid;
@@ -26,17 +26,26 @@ typedef struct{
 } client_t;
 
 client_t *clients[MAX_CLIENTS];
-
 pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER;
 
-void str_overwrite_stdout() {
-    printf("\r%s", "> ");
-    fflush(stdout);
-}
+// Color codes
+const char* colors[] = {
+    "\033[31m", // Red
+    "\033[32m", // Green  
+    "\033[33m", // Yellow
+    "\033[34m", // Blue
+    "\033[35m", // Magenta
+    "\033[36m", // Cyan
+    "\033[91m", // Light Red
+    "\033[92m", // Light Green
+    "\033[93m", // Light Yellow
+    "\033[94m", // Light Blue
+    "\033[95m", // Light Magenta
+    "\033[96m", // Light Cyan
+};
 
-void str_trim_lf (char* arr, int length) {
-    int i;
-    for (i = 0; i < length; i++) {
+void str_trim_lf(char* arr, int length) {
+    for (int i = 0; i < length; i++) {
         if (arr[i] == '\n') {
             arr[i] = '\0';
             break;
@@ -44,149 +53,115 @@ void str_trim_lf (char* arr, int length) {
     }
 }
 
-void print_client_addr(struct sockaddr_in addr){
-    printf("%d.%d.%d.%d",
-        addr.sin_addr.s_addr & 0xff,
-        (addr.sin_addr.s_addr & 0xff00) >> 8,
-        (addr.sin_addr.s_addr & 0xff0000) >> 16,
-        (addr.sin_addr.s_addr & 0xff000000) >> 24);
-}
-
-/* Add clients to queue */
-void queue_add(client_t *cl){
+/* Add client to queue */
+void queue_add(client_t *cl) {
     pthread_mutex_lock(&clients_mutex);
-
-    for(int i=0; i < MAX_CLIENTS; ++i){
-        if(!clients[i]){
+    for (int i = 0; i < MAX_CLIENTS; ++i) {
+        if (!clients[i]) {
             clients[i] = cl;
             break;
         }
     }
-
     pthread_mutex_unlock(&clients_mutex);
 }
 
-/* Remove clients to queue */
-void queue_remove(int uid){
+/* Remove client from queue */
+void queue_remove(int uid) {
     pthread_mutex_lock(&clients_mutex);
-
-    for(int i=0; i < MAX_CLIENTS; ++i){
-        if(clients[i]){
-            if(clients[i]->uid == uid){
-                clients[i] = NULL;
-                break;
-            }
+    for (int i = 0; i < MAX_CLIENTS; ++i) {
+        if (clients[i] && clients[i]->uid == uid) {
+            clients[i] = NULL;
+            break;
         }
     }
-
     pthread_mutex_unlock(&clients_mutex);
 }
 
 /* Send message to all clients except sender */
-void send_message(char *s, int uid){
+void send_message(char *s, int uid) {
     pthread_mutex_lock(&clients_mutex);
-
-    for(int i=0; i<MAX_CLIENTS; ++i){
-        if(clients[i]){
-            if(clients[i]->uid != uid){
-                if(write(clients[i]->sockfd, s, strlen(s)) < 0){
-                    perror("ERROR: write to descriptor failed");
-                    break;
-                }
+    
+    for (int i = 0; i < MAX_CLIENTS; ++i) {
+        if (clients[i] && clients[i]->uid != uid) {
+            if (write(clients[i]->sockfd, s, strlen(s)) < 0) {
+                perror("ERROR: write to descriptor failed");
             }
         }
     }
-
+    
     pthread_mutex_unlock(&clients_mutex);
 }
 
-/* Get color code for a client */
-const char* get_color_code(int uid) {
-    // Assign colors based on UID to ensure consistency
-    const char* colors[] = {
-        "\033[31m", "\033[32m", "\033[33m", "\033[34m", "\033[35m", "\033[36m",
-        "\033[91m", "\033[92m", "\033[93m", "\033[94m", "\033[95m", "\033[96m"
-    };
-    return colors[uid % 12];
-}
-
 /* Handle all communication with the client */
-void *handle_client(void *arg){
-    char buff_out[BUFFER_SZ];
+void *handle_client(void *arg) {
+    char buffer[BUFFER_SZ];
     char name[32];
     int leave_flag = 0;
 
     cli_count++;
     client_t *cli = (client_t *)arg;
 
-    // Assign a color based on UID
-    strcpy(cli->color_code, get_color_code(cli->uid));
+    // Assign color based on UID
+    strcpy(cli->color_code, colors[cli->uid % 12]);
 
-    // Name
-    if(recv(cli->sockfd, name, 32, 0) <= 0 || strlen(name) < 2 || strlen(name) >= 32-1){
-        printf("Didn't enter the name.\n");
+    // Get client name
+    if (recv(cli->sockfd, name, 32, 0) <= 0 || strlen(name) < 2) {
+        printf("Client didn't enter a valid name.\n");
         leave_flag = 1;
     } else {
+        str_trim_lf(name, strlen(name));
         strcpy(cli->name, name);
-        sprintf(buff_out, "%s%s has joined\033[0m\n", cli->color_code, cli->name);
-        printf("%s", buff_out);
-        send_message(buff_out, cli->uid);
+        
+        // Send join message WITH COLOR
+        sprintf(buffer, "%s%s has joined\033[0m\n", cli->color_code, cli->name);
+        printf("%s has joined\n", cli->name);
+        send_message(buffer, cli->uid);
     }
 
-    bzero(buff_out, BUFFER_SZ);
+    bzero(buffer, BUFFER_SZ);
 
-    while(1){
-        if (leave_flag) {
-            break;
-        }
-
-        int receive = recv(cli->sockfd, buff_out, BUFFER_SZ, 0);
-        if (receive > 0){
-            if(strlen(buff_out) > 0){
-                // Create formatted message with color
-                char formatted_msg[BUFFER_SZ];
-                sprintf(formatted_msg, "%s%s\033[0m", cli->color_code, buff_out);
-                send_message(formatted_msg, cli->uid);
+    while (!leave_flag) {
+        int receive = recv(cli->sockfd, buffer, BUFFER_SZ, 0);
+        
+        if (receive > 0) {
+            if (strlen(buffer) > 0) {
+                // Create colored message
+                char colored_msg[BUFFER_SZ];
+                sprintf(colored_msg, "%s%s\033[0m\n", cli->color_code, buffer);
                 
-                // Remove color codes for server display
-                char clean_msg[BUFFER_SZ];
-                strcpy(clean_msg, buff_out);
-                char *color_start = strstr(clean_msg, "\033[");
-                while (color_start) {
-                    char *color_end = strstr(color_start, "m");
-                    if (color_end) {
-                        memmove(color_start, color_end + 1, strlen(color_end + 1) + 1);
-                    }
-                    color_start = strstr(clean_msg, "\033[");
-                }
-                str_trim_lf(clean_msg, strlen(clean_msg));
-                printf("%s\n", clean_msg);
+                // Send colored message to all clients
+                send_message(colored_msg, cli->uid);
+                
+                // Print plain message on server
+                printf("%s\n", buffer);
             }
         } 
         else if (receive == 0) {
-            sprintf(buff_out, "%s%s has left\033[0m\n", cli->color_code, cli->name);
-            printf("%s", buff_out);
-            send_message(buff_out, cli->uid);
+            // Client left
+            sprintf(buffer, "%s%s has left\033[0m\n", cli->color_code, cli->name);
+            printf("%s has left\n", cli->name);
+            send_message(buffer, cli->uid);
             leave_flag = 1;
         } 
         else {
-            printf("ERROR: -1\n");
+            perror("ERROR: recv failed");
             leave_flag = 1;
         }
 
-        bzero(buff_out, BUFFER_SZ);
+        bzero(buffer, BUFFER_SZ);
     }
 
-    /* Delete client from queue and yield thread */
+    // Cleanup
     close(cli->sockfd);
     queue_remove(cli->uid);
     free(cli);
     cli_count--;
     pthread_detach(pthread_self());
+    
     return NULL;
 }
 
-int main(int argc, char **argv){
+int main(int argc, char **argv) {
     if (argc != 3) {
         printf("Usage: %s <ip> <port>\n", argv[0]);
         return EXIT_FAILURE;
@@ -194,65 +169,57 @@ int main(int argc, char **argv){
 
     char *ip = argv[1];
     int port = atoi(argv[2]);
-    int option = 1;
     int listenfd = 0, connfd = 0;
-    struct sockaddr_in serv_addr;
-    struct sockaddr_in cli_addr;
+    struct sockaddr_in serv_addr, cli_addr;
     pthread_t tid;
 
-    /* Socket settings */
+    // Socket settings
     listenfd = socket(AF_INET, SOCK_STREAM, 0);
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = inet_addr(ip);
     serv_addr.sin_port = htons(port);
 
-    /* Ignore pipe signals */
     signal(SIGPIPE, SIG_IGN);
 
-    if(setsockopt(listenfd, SOL_SOCKET,(SO_REUSEPORT | SO_REUSEADDR),(char*)&option,sizeof(option)) < 0){
-        perror("ERROR: setsockopt failed");
-        return EXIT_FAILURE;
-    }
+    int option = 1;
+    setsockopt(listenfd, SOL_SOCKET, (SO_REUSEPORT | SO_REUSEADDR), (char*)&option, sizeof(option));
 
-    /* Bind */
-    if(bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+    // Bind
+    if (bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
         perror("ERROR: Socket binding failed");
         return EXIT_FAILURE;
     }
 
-    /* Listen */
+    // Listen
     if (listen(listenfd, 10) < 0) {
         perror("ERROR: Socket listening failed");
         return EXIT_FAILURE;
     }
 
     printf("=== WELCOME TO THE CHATROOM ===\n");
-    printf("Server IP: %s, Port: %d\n\n", ip, port);
+    printf("Server: %s:%d\n\n", ip, port);
 
-    while(1){
+    while (1) {
         socklen_t clilen = sizeof(cli_addr);
         connfd = accept(listenfd, (struct sockaddr*)&cli_addr, &clilen);
 
-        /* Check if max clients is reached */
-        if((cli_count + 1) == MAX_CLIENTS){
-            printf("Max clients reached. Rejected: ");
-            print_client_addr(cli_addr);
-            printf(":%d\n", cli_addr.sin_port);
+        // Check max clients
+        if (cli_count + 1 == MAX_CLIENTS) {
+            printf("Max clients reached. Rejected connection.\n");
             close(connfd);
             continue;
         }
 
-        /* Client settings */
+        // Create client
         client_t *cli = (client_t *)malloc(sizeof(client_t));
         cli->address = cli_addr;
         cli->sockfd = connfd;
         cli->uid = uid++;
 
-        /* Add client to the queue and fork thread */
+        // Add to queue and create thread
         queue_add(cli);
         pthread_create(&tid, NULL, &handle_client, (void*)cli);
 
-        /* Reduce CPU usage */
         sleep(1);
     }
 
